d3060907e5dff91b511827ec6696ae8a3217e3a0
soraabe <ritsuki200311@gmail.com>
2025-04-27 23:42:55 +0900

    Fix comments form and set post in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,24 @@
 class CommentsController < ApplicationController
-    before_action :set_post
+  before_action :set_post
   
-    def create
-        @comment = @post.comments.build(comment_params)
-        @comment.user = current_user  # ユーザーを設定
-      
-        if @comment.save
-          redirect_to @post, notice: "コメントが投稿されました！"
-        else
-          redirect_to @post, alert: "コメントの投稿に失敗しました。"
-        end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
-  
-    def set_post
-      @post = Post.find(params[:post_id])
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user  # ユーザーを設定
+
+    if @comment.save
+      redirect_to @post, notice: "コメントが投稿されました！"  # 成功時
+    else
+      redirect_to @post, alert: "コメントの投稿に失敗しました。"  # 失敗時
     end
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,45 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿が作成されました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # @post は :set_post コールバックでセットされます
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿が更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @comment = @post.comments.build  # 新しいコメントのインスタンスを作成
+  end
+
   def destroy
     @post.destroy
-    redirect_to posts_path, notice: '投稿が削除されました。'
+    redirect_to posts_path, notice: "投稿が削除されました。"
   end
 
   private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 
   def set_post
     @post = Post.find(params[:id])

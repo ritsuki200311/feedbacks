fff57c6e34ee33d250115b3a8e43e5860afeddad
soraabe <abesora553@gmail.com>
2025-07-11 01:59:51 +0900

    Configure Active Job queue adapter to async in production

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Hello
     config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.\
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
@@ -23,5 +23,8 @@ module Hello
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Active Jobのキューアダプターをasyncに設定し、Solid Queueのデータベース接続を不要にする
+    config.active_job.queue_adapter = :async
   end
 end

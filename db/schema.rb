4672415bf5177d98975df661125982ad6f252819
soraabe <abesora553@gmail.com>
2025-07-10 10:44:14 +0900

    feat: データベースをPostgreSQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_03_024807) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_06_083258) do
+  create_schema "_heroku"
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+  enable_extension "pg_stat_statements"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -137,36 +143,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_03_024807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "coins", default: 0, null: false
+    t.integer "coins", default: 1, null: false
     t.integer "rank_point", default: 0, null: false
     t.integer "rank_points", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "votable_type", null: false
-    t.integer "votable_id", null: false
-    t.integer "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_votes_on_user_id"
-    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable"
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "entries", "rooms"
-  add_foreign_key "entries", "users"
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
-  add_foreign_key "preferences", "users"
-  add_foreign_key "received_videos", "posts"
-  add_foreign_key "received_videos", "users"
-  add_foreign_key "received_videos", "users", column: "sender_id"
-  add_foreign_key "supporter_profiles", "users"
-  add_foreign_key "votes", "users"
-end

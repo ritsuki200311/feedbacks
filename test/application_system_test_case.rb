42ab3fd3db297b52c5491637b8d44ed004ebb870
soraabe <abesora553@gmail.com>
2025-07-24 23:03:48 +0900

    Fix RuboCop style violation after previous changes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "securerandom"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.add_argument("--headless")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-gpu")

0d84cb5a8bc935af8e09c9f97a9f095f2afca8ff
soraabe <abesora553@gmail.com>
2025-07-24 23:00:54 +0900

    Fix Selenium WebDriver initialization error in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,13 +1,20 @@
 require "test_helper"
+require "securerandom"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
-  Capybara.default_max_wait_time = 5 # 待機時間を5秒に設定
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument("--headless")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--user-data-dir=/tmp/chrome-user-data-#{SecureRandom.uuid}")
+  end
+  Capybara.default_max_wait_time = 5
 
   def sign_in(user)
     visit new_user_session_path
     fill_in "Email", with: user.email
-    fill_in "Password", with: "password" # users.yml のパスワードに合わせてください
+    fill_in "Password", with: "password"
     click_on "Log in"
   end
 end

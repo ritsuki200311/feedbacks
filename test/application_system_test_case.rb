60c3a0e491ca79fe9b6fb0835c3da494a0989444
soraabe <abesora553@gmail.com>
2025-07-24 23:36:32 +0900

    Fix Selenium configuration for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,20 +2,17 @@ require "test_helper"
 require "securerandom"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
-    capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      "goog:chromeOptions" => {
-        "args" => [
-          "headless",
-          "disable-gpu",
-          "no-sandbox",
-          "window-size=1400,1400",
-          "disable-dev-shm-usage",
-          "user-data-dir=/tmp/chrome-user-data-#{Process.pid}"
-        ]
-      }
-    )
-  }
+  chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument("--headless")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--window-size=1400,1400")
+    opts.add_argument("--user-data-dir=/tmp/chrome-user-data-#{Process.pid}")
+  end
+
+  driven_by :selenium, using: :chrome, options: chrome_options
+
   Capybara.default_max_wait_time = 5
 
   def sign_in(user)

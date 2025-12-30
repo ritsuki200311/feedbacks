34f9fed2609ed4ee6bb3283ee1df2a9742c901b3
Ritsuki <asdfas@my.mac.local>
2025-12-30 14:11:13 +0900

    Fix HTML email delivery with Resend API

--- lib/resend_delivery_method.rb
+++ lib/resend_delivery_method.rb
@@ -6,17 +6,32 @@ class ResendDeliveryMethod
   end
 
   def deliver!(mail)
+    # メールがマルチパート（HTML + テキスト）の場合
+    if mail.multipart?
+      html_body = mail.html_part&.body&.decoded
+      text_body = mail.text_part&.body&.decoded
+    else
+      # シングルパートの場合
+      if mail.content_type&.include?('text/html')
+        html_body = mail.body.decoded
+        text_body = nil
+      else
+        html_body = nil
+        text_body = mail.body.decoded
+      end
+    end
+
     params = {
       from: mail.from.first,
       to: mail.to,
-      subject: mail.subject,
-      html: mail.html_part&.body&.decoded || mail.body.decoded
+      subject: mail.subject
     }
 
+    # HTMLパートがあれば追加
+    params[:html] = html_body if html_body.present?
+
     # テキストパートがあれば追加
-    if mail.text_part
-      params[:text] = mail.text_part.body.decoded
-    end
+    params[:text] = text_body if text_body.present?
 
     # Resend APIでメール送信
     Resend::Emails.send(params)
